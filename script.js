3656aab7a0e167ed98ae067daaf402fefd0a52f8
Oran <oranl@wearemove.io>
2022-12-11 13:29:56 +0200

    added photos and carousel

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function openHamburger(){
     }
 }
 //https://splidejs.com/
+document.addEventListener(
+    'DOMContentLoaded', function(){
+        new Splide('.splide',{
+            type: 'loop',
+            perPage: 3,
+            focus: 'center',
+            autoplay: true,
+            interval: 5000,
+            // flickMaxPages: 3,
+            // updateOnMove: true,
+            pagination: true,
+            padding: '10%',
+            throttle: 300,
+            breakpoints: {
+                1000: {
+                    perPage: 1,
+                    padding: '15%'
+                }
+            }
+        }).mount()
+    }
+)
