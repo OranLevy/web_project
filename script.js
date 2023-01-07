20ef6293d692e76fb2775ab2547fcd18ea01180f
Oran <oranl@wearemove.io>
2023-01-07 14:30:56 +0200

    shop page update

--- script.js
+++ script.js
@@ -13,12 +13,13 @@ function getProducts(){
             console.log(request);
             let data = JSON.parse(request.responseText)
             console.log(data);
-            let imgElems = document.getElementsByClassName('prod-img');
+            let imgElems = document.querySelectorAll('.prod-img img');
             let titleElems = document.getElementsByClassName('prod-title');
             let priceElems = document.getElementsByClassName('prod-price');
             let descElems = document.getElementsByClassName('prod-desc-txt');
             for(let i = 0; i < data.length; i++){
-                imgElems[i].innerHTML = data[i].image;
+                console.log(imgElems[i]);
+                imgElems[i].setAttribute('src', data[i].image);
                 titleElems[i].innerHTML = data[i].name;
                 priceElems[i].innerHTML = data[i].price + '$';
                 descElems[i].innerHTML = data[i].description;
@@ -28,6 +29,22 @@ function getProducts(){
     request.open('POST', 'shop.php', true);
     request.setRequestHeader('Content-type', 'application/json');
     request.send();
+}
+
+function fixWidthImg(){
+    let imgElems = document.querySelectorAll('.prod-img img');
+    for(let i = 0; i < imgElems.length; i++){
+        if(imgElems[i].hasAttribute('src')){
+            let attr = imgElems[i].getAttribute('src');
+            console.log(attr);
+            if(attr.includes('jigger')){
+                imgElems[i].style.width = '50%';
+            }
+            if(attr.includes('pourer')){
+                imgElems[i].style.width = '35%';
+            }
+        }
+    }
 }
 //https://splidejs.com/
 document.addEventListener(
