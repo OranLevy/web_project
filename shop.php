e08818064aec8ff6924310aaa458ea8682f47680
Oran <oranl@wearemove.io>
2023-01-12 19:35:06 +0200

    fixes

--- shop.php
+++ shop.php
@@ -7,13 +7,9 @@ global $database;
 //    die('Connection failed');
 //}
 $products = Product::fetch_products();
-//var_dump($products);
 $response = Array();
 foreach ($products as $prod){
-//    var_dump($prod);
     $response[] = array('id' => utf8_encode($prod->id), 'name' => utf8_encode($prod->name), 'price' => utf8_encode($prod->price), 'description' => utf8_encode($prod->description), 'image' => utf8_encode($prod->image));
 }
-//var_dump($response);
 $response = json_encode($response);
-//var_dump($response);
 print_r($response);
